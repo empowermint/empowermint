db18d8ec7879cfa9fb7ab9cbff7abb04c2bdc5d9
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-02-18 18:04:50 +0000

    Sketch site structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { MainMenu } from '@/components/mainMenu/MainMenu'
 
 export default function RootLayout({
   children,
@@ -7,12 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header>
+          <MainMenu />
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-import Image from 'next/image'
 import { Poppins } from '@next/font/google'
-import styles from './page.module.css'
+import { SkillList } from '@/components/skillList/SkillList'
 
 const inter = Poppins({ weight: ['300'], style: ['normal'], subsets: ['latin'] })
 
 export default function Home() {
+
   return (
-    <main className={styles.main}>
-      
+    <main>
+      <section className='hero'>
+        <h1>James T. Harding</h1>
+        <p>Software Engineer</p>
+      </section>
+      <section className='colors-secondary full-width skill-list'>
+        <SkillList />
+      </section>
+      <section>
+        <h2>Projects</h2>
+      </section>
     </main>
   )
 }
