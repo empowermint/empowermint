16b79cfa2ade477570982f7c1128b4dc333e1f64
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-02-19 14:03:58 +0000

    Implement contact menu

--- components/mainMenu/MainMenu.tsx
+++ components/mainMenu/MainMenu.tsx
@@ -4,7 +4,7 @@ export function MainMenu() {
 
   return (
     <>
-      <nav id='menu-main' className={styles.mainMenu}>
+      <nav id='menu-main' className={`${styles.menu} ${styles.menu_main}`}>
         <ul>
           <li>
             <a href='#section-hero'>Home</a>
@@ -21,6 +21,22 @@ export function MainMenu() {
               className={styles.featuredMenuItem}>
                 Contact
             </a>
+          </li>
+        </ul>
+      </nav>
+      <nav id='menu-contact' className={`${styles.menu} ${styles.menu_contact}`}>
+        <ul>
+          <li>
+            <a href='mailto:j.t.harding@gmail.com'>Email</a>
+          </li>
+          <li>
+            <a href='https://github.com/empowermint'>GitHub</a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/in/james-t-harding-29b5188/'>LinkedIn</a>
+          </li>
+          <li>
+            <a href='https://codepen.io/empowermint'>CodePen</a>
           </li>
         </ul>
       </nav>
