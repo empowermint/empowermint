c260313f5b9b3f36929b16bb50bd058bb87283a1
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-02-19 13:37:00 +0000

    Add menu corner

--- components/mainMenu/MainMenu.tsx
+++ components/mainMenu/MainMenu.tsx
@@ -3,25 +3,27 @@ import styles from './mainMenu.module.css'
 export function MainMenu() {
 
   return (
-    <nav id='menu-main' className={styles.mainMenu}>
-      <ul>
-        <li>
-          <a href='#section-hero'>Home</a>
-        </li>
-        <li>
-          <a href='#section-skills'>Skills</a>
-        </li>
-        <li>
-          <a href='#section-projects'>Projects</a>
-        </li>
-        <li>
-          <a
-            href='#section-contact'
-            className={styles.featuredMenuItem}>
-              Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav id='menu-main' className={styles.mainMenu}>
+        <ul>
+          <li>
+            <a href='#section-hero'>Home</a>
+          </li>
+          <li>
+            <a href='#section-skills'>Skills</a>
+          </li>
+          <li>
+            <a href='#section-projects'>Projects</a>
+          </li>
+          <li>
+            <a
+              href='#section-contact'
+              className={styles.featuredMenuItem}>
+                Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
