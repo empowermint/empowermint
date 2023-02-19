0f7863fe6b14885922526c84576f87ff9b1b1370
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-02-19 21:33:37 +0000

    Implement icons

--- components/mainMenu/MainMenu.tsx
+++ components/mainMenu/MainMenu.tsx
@@ -1,4 +1,5 @@
 import styles from './mainMenu.module.css'
+import { IconSend, IconBrandGithub, IconBrandLinkedin, IconBrandCodepen } from '@tabler/icons-react';
 
 export function MainMenu() {
 
@@ -27,16 +28,28 @@ export function MainMenu() {
       <nav id='menu-contact' className={`${styles.menu} ${styles.menu_contact}`}>
         <ul>
           <li>
-            <a href='mailto:j.t.harding@gmail.com'>Email</a>
+            <a href='mailto:j.t.harding@gmail.com'>
+              <IconSend />
+              <span className='visually-hidden'>Email</span>
+            </a>
           </li>
           <li>
-            <a href='https://github.com/empowermint'>GitHub</a>
+            <a href='https://github.com/empowermint'>
+              <IconBrandGithub />
+              <span className='visually-hidden'>GitHub</span>
+            </a>
           </li>
           <li>
-            <a href='https://www.linkedin.com/in/james-t-harding-29b5188/'>LinkedIn</a>
+            <a href='https://www.linkedin.com/in/james-t-harding-29b5188/'>
+              <IconBrandLinkedin />
+              <span className='visually-hidden'>LinkedIn</span>
+            </a>
           </li>
           <li>
-            <a href='https://codepen.io/empowermint'>CodePen</a>
+            <a href='https://codepen.io/empowermint'>
+              <IconBrandCodepen />
+              <span className='visually-hidden'>CodePen</span>
+            </a>
           </li>
         </ul>
       </nav>
