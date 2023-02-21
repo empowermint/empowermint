190f55824b9edd13fef8365c30fcabea79b4f2bd
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-02-21 18:21:10 +0000

    Add for-now contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Poppins } from '@next/font/google'
 import { SkillList } from '@/components/skillList/SkillList'
 import { ProjectsList } from '@/components/projectsList/ProjectsList' 
+import { IconMail } from '@tabler/icons-react'
 
 const inter = Poppins({ weight: ['300'], style: ['normal'], subsets: ['latin'] })
 
@@ -19,8 +20,12 @@ export default function Home() {
         <h2>Projects</h2>
         <ProjectsList />
       </section>
-      <section id='section-contact'>
-        <h2>Contact</h2>
+      <section id='section-contact' className='colors-secondary full-width-bleed center'>
+        <div>
+          <h2>Contact</h2>
+          <IconMail />
+          <p>j.t.harding@gmail.com</p>
+        </div>
       </section>
     </main>
   )
