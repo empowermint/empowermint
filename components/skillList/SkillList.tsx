36524d0fa110a2c13972e4b9b83b15f118c280e3
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-10-29 11:52:01 +0000

    Extend skill list

--- components/skillList/SkillList.tsx
+++ components/skillList/SkillList.tsx
@@ -1,28 +1,49 @@
-import styles from './skillList.module.css'
-import { IconBrandHtml5, IconBrandCss3, IconBrandJavascript, IconBrandReact, IconBrandNextjs, IconBrandTypescript, IconBrandGit, IconStethoscope } from '@tabler/icons-react'
+import styles from "./skillList.module.css";
+import {
+  IconBrandHtml5,
+  IconBrandCss3,
+  IconBrandJavascript,
+  IconBrandReact,
+  IconBrandNextjs,
+  IconBrandTypescript,
+  IconBrandGit,
+  IconStethoscope,
+  IconCloudUpload,
+  IconCirclesRelation,
+  IconBrandAmazon,
+  IconBrandCucumber,
+  IconLambda,
+} from "@tabler/icons-react";
 
 export function SkillList() {
   const skills = [
-    {name: 'HTML', icon: <IconBrandHtml5 />},
-    {name: 'CSS', icon: <IconBrandCss3 />},
-    {name: 'JS', icon: <IconBrandJavascript />},
-    {name: 'React', icon: <IconBrandReact />},
-    {name: 'Next.js', icon: <IconBrandNextjs />},
-    {name: 'TypeScript', icon: <IconBrandTypescript />},
-    {name: 'Git', icon: <IconBrandGit />},
-    {name: 'TDD', icon: <IconStethoscope />},
-  ]
+    { name: "AWS", icon: <IconBrandAmazon /> },
+    { name: "TypeScript", icon: <IconBrandTypescript /> },
+    { name: "React", icon: <IconBrandReact /> },
+    { name: "Next.js", icon: <IconBrandNextjs /> },
+    { name: "CI/CD", icon: <IconCirclesRelation /> },
+    { name: "IaC", icon: <IconCloudUpload /> },
+    { name: "Serverless", icon: <IconLambda /> },
+    { name: "TDD", icon: <IconStethoscope /> },
+    { name: "Cucumber", icon: <IconBrandCucumber /> },
+    { name: "HTML", icon: <IconBrandHtml5 /> },
+    { name: "(S)CSS", icon: <IconBrandCss3 /> },
+    { name: "JavaScript", icon: <IconBrandJavascript /> },
+    { name: "Git", icon: <IconBrandGit /> },
+  ];
 
   return (
     <div className={styles.wrapper}>
       <ul className={styles.skillList}>
         {skills.map((skill, i) => {
-          return <li key={i}>
-            {skill.icon}
-            {skill.name}
-          </li>
+          return (
+            <li key={i}>
+              {skill.icon}
+              {skill.name}
+            </li>
+          );
         })}
       </ul>
     </div>
-   )
+  );
 }
